5406f3db23b155bfaf19d27ec343c5f328f9787c
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-09-17 02:31:04 -0700

    stuck on making donut change

--- js/FCOdonut.js
+++ js/FCOdonut.js
@@ -1,10 +1,12 @@
 function cast(d) {
-  d.num = +d.num;
-  d.place = +d.place;
+  d.num = +d.num || 0;
+  d.place = +d.place || 0;
   return d;
 }
 
 var donutsvg = d3.select('#FCOdonutgraph')
+	.append('g')
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
 donutsvg.append("g")
 	.attr("class", "slices");
@@ -15,29 +17,30 @@ donutsvg.append("g")
 donutsvg.append("g")
 	.attr("class", "lines");
 
-var width = 960,
-height = 450,
-radius = Math.min(width, height) / 2;
+var donutwidth = 960,
+donutheight = 450,
+donutradius = Math.min(width, height) / 2;
 
-var pie = d3.layout.pie()
+var pie = d3.pie()
 	.sort(null)
 	.value(function(d) {
 		return d.value;
 });
 
-var arc = d3.svg.arc()
-	.outerRadius(radius * 0.8)
-	.innerRadius(radius * 0.4);
+var arc = d3.arc()
+	.outerRadius(donutradius * 0.8)
+	.innerRadius(donutradius * 0.4);
 
-var legendRectSize = (radius * 0.05);
-var legendSpacing = radius * 0.02;
+var legendRectSize = (donutradius * 0.05);
+var legendSpacing = donutradius * 0.02;
 
 var donutTip = d3.select("#FCOdonut")
 	.append("div")
 	.attr("class", "toolTip");
 
 var donutColorScale = d3.scaleLinear()
-	.range(["white", "#08306b"]);
+	.domain([1,16])
+	.range(["#08306b", "white"]);
 
 
 d3.selectAll("input")
@@ -110,6 +113,7 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 				break;
 		}
 	}
+
 	/*console.log(nofirstcallouts);
 	console.log(onefirstcallouts);
 	console.log(twofirstcallouts);
@@ -123,10 +127,19 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 		.rollup(function(d) { 
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(nofirstcallouts);
+	
 	sumnofirstcallouts = sumnofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumnofirstcallouts = sumnofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumnofirstcallouts, function(d) { return d.value; })} })
 	sumnofirstcallouts = sumnofirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumnofirstcallouts.map(function(d) { return +d.key});
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumnofirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumnofirstcallouts);
 
 	sumonefirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -134,9 +147,18 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(onefirstcallouts);
 	sumonefirstcallouts = sumonefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumonefirstcallouts = sumonefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumonefirstcallouts, function(d) { return d.value; })}})
 	sumonefirstcallouts = sumonefirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumonefirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumonefirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumonefirstcallouts);
 
 	sumtwofirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -144,9 +166,18 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(twofirstcallouts);
 	sumtwofirstcallouts = sumtwofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumtwofirstcallouts = sumtwofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumtwofirstcallouts, function(d) { return d.value;})}})
 	sumtwofirstcallouts = sumtwofirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumtwofirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumtwofirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumtwofirstcallouts);
 
 	sumthreefirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -154,9 +185,18 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(threefirstcallouts);
 	sumthreefirstcallouts = sumthreefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumthreefirstcallouts = sumthreefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumthreefirstcallouts, function(d) { return d.value;})}})
 	sumthreefirstcallouts = sumthreefirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumthreefirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumthreefirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumthreefirstcallouts);
 
 	sumfourfirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -164,9 +204,18 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(fourfirstcallouts);
 	sumfourfirstcallouts = sumfourfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumfourfirstcallouts = sumfourfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumfourfirstcallouts, function(d) { return d.value;})}})
 	sumfourfirstcallouts = sumfourfirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumfourfirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumfourfirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumfourfirstcallouts);
 
 	sumfivefirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -174,9 +223,18 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(fivefirstcallouts);
 	sumfivefirstcallouts = sumfivefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumfivefirstcallouts = sumfivefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumfivefirstcallouts, function(d) { return d.value;})}})
 	sumfivefirstcallouts = sumfivefirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumfivefirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumfivefirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumfivefirstcallouts);
 
 	sumsixfirstcallouts = d3.nest()
 		.key(function(d) { return +d.place;})
@@ -184,17 +242,26 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 			return d3.sum(d, function(g) {return 1; });
 	}).entries(sixfirstcallouts);
 	sumsixfirstcallouts = sumsixfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
+	sumsixfirstcallouts = sumsixfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumsixfirstcallouts, function(d) { return d.value;})}})
 	sumsixfirstcallouts = sumsixfirstcallouts.sort(function(x, y){
   		 return d3.ascending(x.key, y.key);
 	})
+	keysonly = sumsixfirstcallouts.map(function(d) { return +d.key});
+	console.log(keysonly);
+	for (var i = 1; i < 17; i++) {
+		if (keysonly.indexOf(i) == -1) {
+			sumsixfirstcallouts.splice(i-1, 0, {key: i, value: 0})
+		}
+	}
+	console.log(sumsixfirstcallouts);
 
-	console.log(sumnofirstcallouts);
+	/*console.log(sumnofirstcallouts);
 	console.log(sumonefirstcallouts);
 	console.log(sumtwofirstcallouts);
 	console.log(sumthreefirstcallouts);
 	console.log(sumfourfirstcallouts);
 	console.log(sumfivefirstcallouts);
-	console.log(sumsixfirstcallouts);
+	console.log(sumsixfirstcallouts);*/
 	
 	d3.selectAll("input")
 		.on("change", selectDataset);
@@ -231,40 +298,44 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 	
 	change(sumnofirstcallouts);
 	function change(data) {
+		/* ------- PIE SLICES -------*/
+		var slice = donutsvg.select(".slices").datum(data).selectAll("path.slice")
+	        .data(pie);//, function(d){ return d.data.key });
 
-	/* ------- PIE SLICES -------*/
-	var slice = donutsvg.select(".slices").selectAll("path.slice")
-        .data(pie(data), function(d){ return d.data.key });
+	    donutColorScale.domain(d3.extent(data, function(d) { return d.key; }))
+	    slice.enter()
+	        .insert("path")
+	        .style("fill", function(d) { return donutColorScale(d.data.key); })
+	        .attr("class", "slice")
+	        .attr('d',arc);
 
-    slice.enter()
-        .insert("path")
-        .style("fill", function(d) { return donutColorScale(d.data.key); })
-        .attr("class", "slice");
 
-    slice
-        .transition().duration(1000)
-        .attrTween("d", function(d) {
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                return arc(interpolate(t));
-            };
-        })
-    slice
-        .on("mouseover", function(d){
-            donutTip.style("left", d3.event.pageX+10+"px");
-            donutTip.style("top", d3.event.pageY-25+"px");
-            donutTip.style("display", "inline-block");
-            donutTip.html((d.data.label)+"<br>"+(d.data.value)+"%");
-        });
-    slice
-        .on("mouseout", function(d){
-            div.style("display", "none");
-        });
+	    /*slice
+	        .transition().duration(1000)
+	        .attrTween("d", function(d) {
+	        	console.log("getting here");
+	            this._current = this._current || d;
+	            console.log(this._current);
+	            var interpolate = d3.interpolate(this._current, d);
+	            this._current = interpolate(0);
+	            return function(t) {
+	                return arc(interpolate(t));
+	            };
+	        })*/
+	    /*slice
+	        .on("mouseover", function(d){
+	            donutTip.style("left", d3.event.pageX+10+"px");
+	            donutTip.style("top", d3.event.pageY-25+"px");
+	            donutTip.style("display", "inline-block");
+	            donutTip.html((d.data.label)+"<br>"+(d.data.value)+"%");
+	        });
+	    slice
+	        .on("mouseout", function(d){
+	            div.style("display", "none");
+	        });*/
 
-    slice.exit()
-        .remove();
+	    slice.exit()
+	        .remove();
 
     /*var legend = svg.selectAll('.legend')
         .data(color.domain())
@@ -292,7 +363,7 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 
     /* ------- TEXT LABELS -------*/
 
-    var text = donutsvg.select(".labelName").selectAll("text")
+    /*var text = donutsvg.select(".labelName").selectAll("text")
         .data(pie(data), function(d){ return d.data.label });
 
     text.enter()
@@ -315,7 +386,7 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
             return function(t) {
                 var d2 = interpolate(t);
                 var pos = outerArc.centroid(d2);
-                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+                pos[0] = donutradius * (midAngle(d2) < Math.PI ? 1 : -1);
                 return "translate("+ pos +")";
             };
         })
@@ -334,7 +405,7 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
 
 
     text.exit()
-        .remove();
+        .remove();*/
 
     /* ------- SLICE TO TEXT POLYLINES -------*/
 
@@ -352,14 +423,14 @@ d3.csv("data/allFCOs.csv", cast, function(error, data) {
             return function(t) {
                 var d2 = interpolate(t);
                 var pos = outerArc.centroid(d2);
-                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                pos[0] = donutradius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
                 return [arc.centroid(d2), outerArc.centroid(d2), pos];
             };
         });
 
     polyline.exit()
         .remove();*/
-};
+	};
 
 	
 });
