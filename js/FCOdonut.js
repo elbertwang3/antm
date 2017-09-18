72f3a166c26a966d57b9194421cb7af4a73b55d3
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-09-18 00:56:11 -0700

    donut showing up

--- js/FCOdonut.js
+++ js/FCOdonut.js
@@ -4,9 +4,13 @@ function cast(d) {
   return d;
 }
 
+var donutwidth = 900,
+donutheight = 450,
+donutradius = Math.min(donutwidth, donutheight) / 2;
+
 var donutsvg = d3.select('#FCOdonutgraph')
 	.append('g')
-    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+    .attr('transform', 'translate(' + donutwidth / 2 + ',' + donutheight / 2 + ')');
 
 donutsvg.append("g")
 	.attr("class", "slices");
@@ -17,9 +21,7 @@ donutsvg.append("g")
 donutsvg.append("g")
 	.attr("class", "lines");
 
-var donutwidth = 960,
-donutheight = 450,
-donutradius = Math.min(width, height) / 2;
+
 
 var pie = d3.pie()
 	.sort(null)
@@ -43,394 +45,93 @@ var donutColorScale = d3.scaleLinear()
 	.range(["#08306b", "white"]);
 
 
-d3.selectAll("input")
-	.on("change", selectDataset);
-	
-function selectDataset() {
-	var value = this.value;
-	switch(value) {
-		case "zero":
-			change(sumnofirstcallouts);
-			break;
-		case "one":
-			change(sumonefirstcallouts)
-			break;
-		case "two":
-			change(sumtwofirstcallouts)
-			break;
-		case "three":
-			change(sumthreefirstcallouts)
-			break;
-		case "four":
-			change(sumfourfirstcallouts)
-			break;
-		case "five":
-			change(sumfivefirstcallouts)
-			break;
-		case "six":
-			change(sumsixfirstcallouts)
-			break;
-		default:
-			console.log("should not get here");
-			break;
-	}
-}
+
 d3.csv("data/allFCOs.csv", cast, function(error, data) {
-	//console.log(data);
-	nofirstcallouts = []
-	onefirstcallouts = []
-	twofirstcallouts = []
-	threefirstcallouts = []
-	fourfirstcallouts = []
-	fivefirstcallouts = []
-	sixfirstcallouts = []
+	numfirstcallouts = []
+	sumnumfirstcallouts = []
+	for (var i = 0; i < 7; i++) {
+		numfirstcallouts.push([])
+		//sumnumfirstcallouts.push([])
+	}
 
 	for (var i = 0; i < data.length; i++) {
-		switch(data[i].num) {
-			case 0:
-				nofirstcallouts.push(data[i]);
-				break;
-			case 1:
-				onefirstcallouts.push(data[i]);
-				break;
-			case 2:
-				twofirstcallouts.push(data[i]);
-				break;
-			case 3:
-				threefirstcallouts.push(data[i]);
-				break;
-			case 4:
-				fourfirstcallouts.push(data[i]);
-				break;
-			case 5:
-				fivefirstcallouts.push(data[i]);
-				break;
-			case 6:
-				sixfirstcallouts.push(data[i]);
-				break;
-			default:
-				console.log("should not get here");
-				break;
-		}
+		numfirstcallouts[data[i].num].push(data[i]);
 	}
 
-	/*console.log(nofirstcallouts);
-	console.log(onefirstcallouts);
-	console.log(twofirstcallouts);
-	console.log(threefirstcallouts);
-	console.log(fourfirstcallouts);
-	console.log(fivefirstcallouts);
-	console.log(sixfirstcallouts);*/
-
-	sumnofirstcallouts = d3.nest()
+	for (var i = 0; i < numfirstcallouts.length; i++) {
+		sumnumfirstcallouts.push(d3.nest()
 		.key(function(d) { return +d.place;})
 		.rollup(function(d) { 
 			return d3.sum(d, function(g) {return 1; });
-	}).entries(nofirstcallouts);
+		}).entries(numfirstcallouts[i]));
+		sumnumfirstcallouts[i] = sumnumfirstcallouts[i].map(function(d) { return {key: +d.key, value: +d.value} })
+		//sumnumfirstcallouts[i]= sumnumfirstcallouts[i].map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumnumfirstcallouts[i], function(d) { return d.value; })} })
+		sumnumfirstcallouts[i] = sumnumfirstcallouts[i].sort(function(x, y){
+	  		return d3.ascending(x.key, y.key);
+		})
+		keysonly = sumnumfirstcallouts[i].map(function(d) { return +d.key});
+		for (var j = 1; j < 17; j++) {
+			if (keysonly.indexOf(j) == -1) {
+				sumnumfirstcallouts[i].splice(j-1, 0, {key: j, value: 0})
+			}
+		}
+	}
+	//console.log(sumnumfirstcallouts)
+	var slice = d3.select(".slices").datum(sumnumfirstcallouts[4]).selectAll("path.slice")
+	        .data(pie);//, function(d){ return d.data.key });
+	    slice.enter()
+	        .insert("path")
+	        .style("fill", function(d) { return donutColorScale(d.data.key); })
+	        .attr("class", "slice")
+	        .attr('d',arc)
+
 	
-	sumnofirstcallouts = sumnofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumnofirstcallouts = sumnofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumnofirstcallouts, function(d) { return d.value; })} })
-	sumnofirstcallouts = sumnofirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumnofirstcallouts.map(function(d) { return +d.key});
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumnofirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumnofirstcallouts);
-
-	sumonefirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(onefirstcallouts);
-	sumonefirstcallouts = sumonefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumonefirstcallouts = sumonefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumonefirstcallouts, function(d) { return d.value; })}})
-	sumonefirstcallouts = sumonefirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumonefirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumonefirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumonefirstcallouts);
-
-	sumtwofirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(twofirstcallouts);
-	sumtwofirstcallouts = sumtwofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumtwofirstcallouts = sumtwofirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumtwofirstcallouts, function(d) { return d.value;})}})
-	sumtwofirstcallouts = sumtwofirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumtwofirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumtwofirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumtwofirstcallouts);
-
-	sumthreefirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(threefirstcallouts);
-	sumthreefirstcallouts = sumthreefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumthreefirstcallouts = sumthreefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumthreefirstcallouts, function(d) { return d.value;})}})
-	sumthreefirstcallouts = sumthreefirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumthreefirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumthreefirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumthreefirstcallouts);
-
-	sumfourfirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(fourfirstcallouts);
-	sumfourfirstcallouts = sumfourfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumfourfirstcallouts = sumfourfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumfourfirstcallouts, function(d) { return d.value;})}})
-	sumfourfirstcallouts = sumfourfirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumfourfirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumfourfirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumfourfirstcallouts);
-
-	sumfivefirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(fivefirstcallouts);
-	sumfivefirstcallouts = sumfivefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumfivefirstcallouts = sumfivefirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumfivefirstcallouts, function(d) { return d.value;})}})
-	sumfivefirstcallouts = sumfivefirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumfivefirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumfivefirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumfivefirstcallouts);
-
-	sumsixfirstcallouts = d3.nest()
-		.key(function(d) { return +d.place;})
-		.rollup(function(d) { 
-			return d3.sum(d, function(g) {return 1; });
-	}).entries(sixfirstcallouts);
-	sumsixfirstcallouts = sumsixfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value} })
-	sumsixfirstcallouts = sumsixfirstcallouts.map(function(d) { return {key: +d.key, value: +d.value/d3.sum(sumsixfirstcallouts, function(d) { return d.value;})}})
-	sumsixfirstcallouts = sumsixfirstcallouts.sort(function(x, y){
-  		 return d3.ascending(x.key, y.key);
-	})
-	keysonly = sumsixfirstcallouts.map(function(d) { return +d.key});
-	console.log(keysonly);
-	for (var i = 1; i < 17; i++) {
-		if (keysonly.indexOf(i) == -1) {
-			sumsixfirstcallouts.splice(i-1, 0, {key: i, value: 0})
-		}
-	}
-	console.log(sumsixfirstcallouts);
-
-	/*console.log(sumnofirstcallouts);
-	console.log(sumonefirstcallouts);
-	console.log(sumtwofirstcallouts);
-	console.log(sumthreefirstcallouts);
-	console.log(sumfourfirstcallouts);
-	console.log(sumfivefirstcallouts);
-	console.log(sumsixfirstcallouts);*/
 	
 	d3.selectAll("input")
 		.on("change", selectDataset);
 	
 	function selectDataset() {
-		var value = this.value;
-		switch(value) {
-			case "zero":
-				change(sumnofirstcallouts);
-				break;
-			case "one":
-				change(sumonefirstcallouts)
-				break;
-			case "two":
-				change(sumtwofirstcallouts)
-				break;
-			case "three":
-				change(sumthreefirstcallouts)
-				break;
-			case "four":
-				change(sumfourfirstcallouts)
-				break;
-			case "five":
-				change(sumfivefirstcallouts)
-				break;
-			case "six":
-				change(sumsixfirstcallouts)
-				break;
-			default:
-				console.log("should not get here");
-				break;
-		}
+		var value = +this.value;
+		change(sumnumfirstcallouts[value])
 	}
 	
-	change(sumnofirstcallouts);
-	function change(data) {
-		/* ------- PIE SLICES -------*/
-		var slice = donutsvg.select(".slices").datum(data).selectAll("path.slice")
-	        .data(pie);//, function(d){ return d.data.key });
+	change(sumnumfirstcallouts[0]);
+	function change(newdata) {
+		console.log(newdata);
+	   	var updateSlice = d3.select('.slices').selectAll('path.slice');
+	     var data0 = slice.data(), // store the current data before updating to the new
+         	data1 = pie(newdata);
+          updateslice = updateSlice.data(data1, key);
 
-	    donutColorScale.domain(d3.extent(data, function(d) { return d.key; }))
-	    slice.enter()
-	        .insert("path")
-	        .style("fill", function(d) { return donutColorScale(d.data.key); })
-	        .attr("class", "slice")
-	        .attr('d',arc);
+        updateSlice.enter().append('path.slice')
+            .each(function(d, i) { this._current = findNeighborArc(i, data0, data1, key) || d; })
+            .attr('fill', function(d) { return donutColorScale(d.data.key); })
+            .attr('d', arc);
+	   	
+	   	updateSlice.exit()
+            .transition()
+            .duration(400)
+            .attrTween("d", arcTween)
+            .remove();
 
+       	updateSlice.transition().duration(400)
+                .attrTween('d', arcTween);
 
-	    /*slice
-	        .transition().duration(1000)
-	        .attrTween("d", function(d) {
-	        	console.log("getting here");
-	            this._current = this._current || d;
-	            console.log(this._current);
-	            var interpolate = d3.interpolate(this._current, d);
-	            this._current = interpolate(0);
-	            return function(t) {
-	                return arc(interpolate(t));
-	            };
-	        })*/
-	    /*slice
-	        .on("mouseover", function(d){
-	            donutTip.style("left", d3.event.pageX+10+"px");
-	            donutTip.style("top", d3.event.pageY-25+"px");
-	            donutTip.style("display", "inline-block");
-	            donutTip.html((d.data.label)+"<br>"+(d.data.value)+"%");
-	        });
-	    slice
-	        .on("mouseout", function(d){
-	            div.style("display", "none");
-	        });*/
-
-	    slice.exit()
-	        .remove();
-
-    /*var legend = svg.selectAll('.legend')
-        .data(color.domain())
-        .enter()
-        .append('g')
-        .attr('class', 'legend')
-        .attr('transform', function(d, i) {
-            var height = legendRectSize + legendSpacing;
-            var offset =  height * color.domain().length / 2;
-            var horz = -3 * legendRectSize;
-            var vert = i * height - offset;
-            return 'translate(' + horz + ',' + vert + ')';
-        });
-
-    legend.append('rect')
-        .attr('width', legendRectSize)
-        .attr('height', legendRectSize)
-        .style('fill', color)
-        .style('stroke', color);
-
-    legend.append('text')
-        .attr('x', legendRectSize + legendSpacing)
-        .attr('y', legendRectSize - legendSpacing)
-        .text(function(d) { return d; });
-
-    /* ------- TEXT LABELS -------*/
-
-    /*var text = donutsvg.select(".labelName").selectAll("text")
-        .data(pie(data), function(d){ return d.data.label });
-
-    text.enter()
-        .append("text")
-        .attr("dy", ".35em")
-        .text(function(d) {
-            return (d.data.label+": "+d.value+"%");
-        });
-
-    function midAngle(d){
-        return d.startAngle + (d.endAngle - d.startAngle)/2;
-    }
-
-    text
-        .transition().duration(1000)
-        .attrTween("transform", function(d) {
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = donutradius * (midAngle(d2) < Math.PI ? 1 : -1);
-                return "translate("+ pos +")";
-            };
-        })
-        .styleTween("text-anchor", function(d){
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                return midAngle(d2) < Math.PI ? "start":"end";
-            };
-        })
-        .text(function(d) {
-            return (d.data.label+": "+d.value+"%");
-        });
-
-
-    text.exit()
-        .remove();*/
-
-    /* ------- SLICE TO TEXT POLYLINES -------*/
-
-    /*var polyline = svg.select(".lines").selectAll("polyline")
-        .data(pie(data), function(d){ return d.data.label });
-
-    polyline.enter()
-        .append("polyline");
-
-    polyline.transition().duration(1000)
-        .attrTween("points", function(d){
-            this._current = this._current || d;
-            var interpolate = d3.interpolate(this._current, d);
-            this._current = interpolate(0);
-            return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = donutradius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-                return [arc.centroid(d2), outerArc.centroid(d2), pos];
-            };
-        });
-
-    polyline.exit()
-        .remove();*/
+   
 	};
 
-	
+	function key(d) {
+        return d.data.key;
+    }
+    function findNeighborArc(i, data0, data1, key) {
+        var d;
+        return (d = findPreceding(i, data0, data1, key)) ? {startAngle: d.endAngle, endAngle: d.endAngle}
+            : (d = findFollowing(i, data0, data1, key)) ? {startAngle: d.startAngle, endAngle: d.startAngle}
+                : null;
+    }
+    function arcTween(d) {
+        var i = d3.interpolate(this._current, d);
+        this._current = i(0);
+        return function(t) { return arc(i(t)); };
+    }
 });
